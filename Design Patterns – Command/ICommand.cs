64075d548e010145dbb67e42548a378e7773b1ac
lamboktulus1379 <lamboktulus1379@gmail.com>
2020-12-03 10:42:06 +0700

    Add project files.

---
+++ Design Patterns – Command/ICommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns___Command
+{
+    public interface ICommand
+    {
+        void ExecuteAction();
+        void UndoAction();
+    }
+}
---
+++ Design Patterns – Command/Product.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Design_Patterns___Command
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+        public Product(string name, int price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public void IncreasePrice(int amount)
+        {
+            Price += amount;
+            Console.WriteLine($"The price for the {Name} has been increased by {amount}$.");
+        }
+
+        public bool DecreasePrice(int amount)
+        {
+            if (amount < Price)
+            {
+                Price -= amount;
+                Console.WriteLine($"The price for the {Name} has been decreased by {amount}$.");
+                return true;
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return $"Current price for the {Name} product iss {Price}$.";
+        }
+    }
+}
